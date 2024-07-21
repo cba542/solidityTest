49c713929620f492fdc74b4d0792dfaa10fadcda
Sam Cheng <cba542@hotmail.com>
2024-07-21 15:13:42 +0800

    Add function addPerson

--- 025SimpleStorage.sol
+++ 025SimpleStorage.sol
@@ -10,11 +10,19 @@ contract PeopleRegistry {
     People[] public person;
 
     constructor(){
-        person.push(People({name:"John", age:30}));
-        person.push(People(35, "Alex"));
-        person.push(People(20, "Jenny"));
+        // person.push(People({name:"John", age:30}));
+        // person.push(People(35, "Alex"));
+        // person.push(People(20, "Jenny"));
+
+        addPerson(30, "John");
+        addPerson(35, "Alex");
+        addPerson(20, "Jenny");
+
     }
     
+    function addPerson(uint _age, string memory _name) public {
+        person.push(People(_age,_name));
+    }
 
 }
 
